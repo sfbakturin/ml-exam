7c125f1d26935213799d09c332f801500ad1b1e4
SAVELIY BAKTURIN <zavierwaffle@gmail.com>
2024-06-14 14:03:52 +0300

    Build theory separately.

--- BuildTheory.sh
+++ BuildTheory.sh
@@ -2,57 +2,18 @@
 
 cd theory || exit 1
 
-echo "Building Theory MDs..."
-
-pandoc "header-includes.yaml" \
-    "T1.md" \
-    "T2.md" \
-    "T3.md" \
-    "T4.md" \
-    "T5.md" \
-    "T6.md" \
-    "T7.md" \
-    "T8.md" \
-    "T9.md" \
-    "T10.md" \
-    "T11.md" \
-    "T12.md" \
-    "T13.md" \
-    "T14.md" \
-    "T15.md" \
-    "T16.md" \
-    "T17.md" \
-    "T18.md" \
-    "T19.md" \
-    "T20.md" \
-    "T21.md" \
-    "T22.md" \
-    "T23.md" \
-    "T24.md" \
-    "T25.md" \
-    "T26.md" \
-    "T27.md" \
-    "T28.md" \
-    "T29.md" \
-    "T30.md" \
-    "T31.md" \
-    "T32.md" \
-    "T33.md" \
-    "T34.md" \
-    "T35.md" \
-    "T36.md" \
-    "T37.md" \
-    "T38.md" \
-    "T39.md" \
-    "T40.md" \
-    "T41.md" \
-    "T42.md" \
-    --pdf-engine=xelatex \
-    -V colorlinks=true \
-    -V linkcolor=blue \
-    -V urlcolor=red \
-    -V toccolor=gray \
-    -V geometry:margin=1in \
-    -V "mainfont:Times New Roman" \
-    -V "monofont:DejaVu Sans Mono" \
-    -o "Theory.pdf"
+for INPUT in *.md; do
+    OUTPUT="${INPUT%.*}.pdf"
+    echo "Building ${INPUT}..."
+    pandoc "${INPUT}" \
+        "header-includes.yaml" \
+        --pdf-engine=xelatex \
+        -V colorlinks=true \
+        -V linkcolor=blue \
+        -V urlcolor=red \
+        -V toccolor=gray \
+        -V geometry:margin=1in \
+        -V "mainfont:Times New Roman" \
+        -V "monofont:DejaVu Sans Mono" \
+        -o "${OUTPUT}" || exit 2
+done
